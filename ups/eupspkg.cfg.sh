f20a21652d260623d18bbff9e967e545b1d55319
Fritz Mueller <fritzm@fritzm.org>
2020-04-21 19:26:23 +0000

    Add eupspkg packaging

---
+++ ups/eupspkg.cfg.sh
@@ -0,0 +1,15 @@
+# EupsPkg config file. Sourced by 'eupspkg'
+
+# Link to libevent
+export LDFLAGS="-L$LIBEVENT_DIR/lib"
+export LIBS="-levent -lm"
+export CPPFLAGS="-I$LIBEVENT_DIR/include"
+
+# Link to Lua
+export LUA_CFLAGS="-I$LUA_DIR/include"
+export LUA_LIBS="-L$LUA_DIR/lib -llua"
+
+# Note: It is assumed that glib2 is installed system-wide and that
+# ./configure will detect it up via pkg-config.
+
+CONFIGURE_OPTIONS="--prefix=$PREFIX --with-lua --with-mysql=$MARIADB_DIR/bin/mysql_config"
