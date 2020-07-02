ce9161a0d2127cacb2648122708b68ca8f9ee5e1
Fritz Mueller <fritzm@slac.stanford.edu>
2020-07-02 00:57:25 -0700

    Enable debug symbols

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -4,4 +4,4 @@ export LIBS="-levent -lm"
 export LUA_CFLAGS="-I$LUA_DIR/include -I$CONDA_PREFIX/include"
 export LUA_LIBS="-L$LUA_DIR/lib -llua"
 
-CONFIGURE_OPTIONS="--prefix=$PREFIX --with-lua --with-mysql=$MARIADB_DIR/bin/mysql_config"
+CONFIGURE_OPTIONS="CPPFLAGS='-DDEBUG' CFLAGS='-Og -g' --prefix=$PREFIX --with-lua --with-mysql=$MARIADB_DIR/bin/mysql_config"
